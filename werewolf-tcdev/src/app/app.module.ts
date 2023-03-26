36d713fee4d5458adbebc771bdba236f17103c68
TCDev <thienchuc.dev@gmail.com>
2023-03-26 09:24:46 +0700

    fix project name

--- werewolf-tcdev/src/app/app.module.ts
+++ werewolf-tcdev/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharacterComponent } from './character/character.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
